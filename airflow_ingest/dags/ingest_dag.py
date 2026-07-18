ea5d8a7aca970e869ee83e0b99015de715aa20b7
Shatayu_Ganvir <shatayuganvir@gmail.com>
2026-07-19 04:48:37 +0530

    - Modify dag to support per pdf processing, replacing need for for-loop. - Use partials properly and make sure its wired with pipeline

--- airflow_ingest/dags/ingest_dag.py
+++ airflow_ingest/dags/ingest_dag.py
@@ -1,4 +1,4 @@
-from airflow.sdk import dag, task, Context,chain
+from airflow.sdk import dag, task, Context
 from airflow.sdk.bases.sensor import PokeReturnValue
 from datetime import datetime
 import json
@@ -13,9 +13,20 @@ import os
     tags=['rag']
 )
 def ingest_arxiv_papers():
+    """DAG to ingest papers from Arxiv. Use arxiv client to retrieve papers from arxiv.
+    Download new papers, split them into chunks, create embeddings and store in qdrant db.
+
+    Returns:
+        None
+    """
     
     @task.sensor(poke_interval=30, timeout=3600, mode="poke")
     def ensure_qdrant_collection() -> PokeReturnValue :
+        """Sensor to ensure if qdrant collection is created
+
+        Returns:
+            PokeReturnValue: Returns true post collection creation
+        """
         from src.data_curator.config import get_settings
         from src.data_curator.vectorstore.qdrant_client import ensure_collection
         
@@ -31,7 +42,7 @@ def ingest_arxiv_papers():
     
     @task
     def load_metadata_json(**context:Context) -> None:
-        """Returns list of already-ingested paper metadata dicts."""
+        """Pushes already-ingested paper metadata dicts into xcom"""
         METADATA_PATH = Path(r"data\metadata\ingested_papers.json")
         if METADATA_PATH.exists():
             ingested_papers = json.loads(METADATA_PATH.read_text(encoding="utf-8"))
@@ -40,54 +51,81 @@ def ingest_arxiv_papers():
         context['ti'].xcom_push(key='ingested_papers',value=ingested_papers)
     
     @task
-    def get_new_papers_from_arxiv(**context:Context) -> None:
+    def get_new_papers_from_arxiv(**context:Context) -> list[dict]:
+        """Finds new papers to process and store in qdrantdb
+
+        Returns:
+            list[dict]: list of paper objects
+        """
         from src.data_curator.ingestion.arxiv_client import search_papers,Paper
         
-        papers:list[Paper] = search_papers(query='robotics')
         already_ingested_papers:list[dict] = context['ti'].xcom_pull(task_ids='load_metadata_json',key='ingested_papers')
         ingested_paper_ids = [ingested_paper['arxiv_id'] for ingested_paper in already_ingested_papers]
+        papers:list[Paper] = search_papers(query='robotics')
         new_papers: list[dict] = [paper.model_dump() for paper in papers if paper.arxiv_id not in ingested_paper_ids]
         return new_papers
     
     @task
-    def process_single_paper(paper,**context:Context):
+    def process_single_paper(paper:dict) -> dict:
+        """Downloads pdf paper locally, parses pdf, create chunks, vectorize them and stores in db.
+
+        Args:
+            paper (dict): paper object
+
+        Returns:
+            dict: same paper object if successful
+        """
         from src.data_curator.ingestion.arxiv_client import download_papers
         from src.data_curator.ingestion.pdf_parser import extract_text
         from src.data_curator.chunking.splitter import recursive_character_split
         from src.data_curator.vectorstore.qdrant_client import upsert_chunks
         from src.data_curator.embeddings.sentence_embedder import embed_texts
         from src.data_curator.ingestion.delete_pdf import delete_pdf
-        from src.data_curator.ingestion.arxiv_client import save_metadata
         from src.data_curator.ingestion.arxiv_client import Paper
         
         RAW_DIR = Path("data/raw")
-        METADATA_PATH = Path(r"data\metadata\ingested_papers.json")
         CLOUD_RUN = os.getenv("CLOUD_RUN", "false").lower() == "true"
         
-        already_ingested_papers:list[dict] = context['ti'].xcom_pull(task_ids='load_metadata_json',key='ingested_papers')
-        
-        paper = Paper(**paper)
-        pdf_path = download_papers(paper, download_dir=RAW_DIR)
+        paper_obj = Paper(**paper)
+        pdf_path = download_papers(paper_obj, download_dir=RAW_DIR)
         text:str = extract_text(pdf_path)
         chunks:list[str] = recursive_character_split(text)
         vectors:list[list[float]] = embed_texts(chunks)
         upsert_chunks(
                 chunks=chunks,
                 vectors=vectors,
-                paper_id=paper.arxiv_id,
-                title=paper.title,
-                authors=paper.authors,
+                paper_id=paper_obj.arxiv_id,
+                title=paper_obj.title,
+                authors=paper_obj.authors,
                 source_pdf_path=str(pdf_path),
                 )
                 
         if CLOUD_RUN:
             delete_pdf(pdf_path=pdf_path)
-            
-        already_ingested_papers.append(paper.model_dump())
         
-        save_metadata(already_ingested_papers, METADATA_PATH)
-        
+        return paper
     
-    ensure_qdrant_collection() >> load_metadata_json() >> get_new_papers_from_arxiv() >> process_single_paper.partial().expand(paper=get_new_papers_from_arxiv())
+    @task(trigger_rule='all_done')
+    def update_metadata(processed_papers:list[dict],**context:Context):
+        """Updates metadata json and adds new processed pdfs (arxiv id, paper title, etc.)
+        Args:
+            processed_papers (list[dict]): processed_papers
+        """
+        from src.data_curator.ingestion.arxiv_client import save_metadata
+        METADATA_PATH = Path(r"data\metadata\ingested_papers.json")
+        processed_papers = [paper for paper in processed_papers if paper is not None]
+        
+        ingested_papers = context['ti'].xcom_pull(task_ids='load_metadata_json',key='ingested_papers')
+        ingested_papers.extend(processed_papers)
+        save_metadata(ingested_papers, METADATA_PATH)
+
+    
+    collection_ensured = ensure_qdrant_collection()
+    metadata = load_metadata_json()
+    new_papers = get_new_papers_from_arxiv()
+    processed_papers  = process_single_paper.partial().expand(paper=new_papers)
+    updated_metadata = update_metadata(processed_papers)
+    
+    collection_ensured >> metadata >> new_papers >> processed_papers >> updated_metadata
 
 ingest_arxiv_papers()
